95c0487eaec2a2dd1dc9532129cf7d688e1dcc2d
kirillsev1 <kirillprinh47@gmail.com>
2023-03-19 23:35:00 +0300

    add tests on bash for server

--- test.sh
+++ test.sh
@@ -6,20 +6,21 @@ PG_PASSWORD=test
 PG_DBNAME=test
 DOCKER_NAME=test
 DDL_NAME=db_init.ddl
+TEMPLATES="first/templates"
 
 # set env variables
-echo "PG_HOST=$PG_HOST" > .env
-echo "PG_PORT=$PG_PORT" >> .env
-echo "PG_USER=$PG_USER" >> .env
-echo "PG_PASSWORD=$PG_PASSWORD" >> .env
-echo "PG_DBNAME=$PG_DBNAME" >> .env
-
+echo "PG_HOST=$PG_HOST" > first/.env
+echo "PG_PORT=$PG_PORT" >> first/.env
+echo "PG_USER=$PG_USER" >> first/.env
+echo "PG_PASSWORD=$PG_PASSWORD" >> first/.env
+echo "PG_DBNAME=$PG_DBNAME" >> first/.env
+echo "TEMPLATES_FILE=$TEMPLATES" >> first/.env
 # set up database
-python3.10 setup_db.py
+python3.10 first/setup_db.py
 
 # server start
 echo "Starting the server"
-python3.10 main.py &
+python3.10 first/main.py &
 
 # real tests
 sleep 2
@@ -33,7 +34,7 @@ echo "simple_GET request:"
 get_code=`curl -s -o /dev/null \
     -X GET \
     -w %{http_code} \
-    http://127.0.0.1:8001/students`
+    http://127.0.0.1:8001/`
 if [[ $get_code -eq $OK ]]
 then
 echo "OK"
@@ -46,10 +47,10 @@ fi
 echo "POST request:"
 post_code=`curl -s -o /dev/null \
     -X POST \
-    -d '{"fname": "a1b2c3d4", "lname":"abcdef", "group_":"1"}' \
+    -d '{"fname": "a1b2c3d4", "lname": "abcdef", "email": "email1@email.com"}'\
     -H "Authorization:admin {$token}"\
     -w %{http_code} \
-    http://127.0.0.1:8001/students`
+    http://127.0.0.1:8001`
 if [[ $post_code -eq $CREATED ]]
 then
 echo "OK"
@@ -63,7 +64,7 @@ echo "query_GET request:"
 get_code=`curl -s -o /dev/null \
     -X GET \
     -w %{http_code} \
-    http://127.0.0.1:8001/students?fname=a1ba1b2c3d42c3d4`
+    http://127.0.0.1:8001?fname=a1ba1b2c3d42c3d4`
 if [[ $get_code -eq $OK ]]
 then
 echo "OK"
@@ -78,7 +79,7 @@ post_code=`curl -s -o /dev/null \
     -X DELETE \
     -H "Authorization:admin {$token}"\
     -w %{http_code} \
-    http://127.0.0.1:8001/students?fname=a1b2c3d4`
+    http://127.0.0.1:8001?fname=a1b2c3d4`
 if [[ $post_code -eq $OK ]]
 then
 echo "OK"
@@ -89,4 +90,4 @@ exit 1
 fi
 
 # # clean up
-rm .env
+rm first/.env
